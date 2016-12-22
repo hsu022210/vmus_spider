1f3f37ca727dba9653f77cc3ea08641f4c1cd99e
Alec Hsu <hsu022210@gmail.com>
2016-12-22 04:37:54 -0800

    url max length

--- vmus_spider_django/dramaList/models.py
+++ vmus_spider_django/dramaList/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 class Show(models.Model):
     show_name = models.CharField(max_length=50)
-    show_url = models.URLField()
-    latest_episode_url = models.URLField()
+    show_url = models.URLField(max_length=300)
+    latest_episode_url = models.URLField(max_length=400)
     latest_episode_name = models.CharField(max_length=100, null=True, blank=True)
     latest_post_time = models.CharField(max_length=50, null=True, blank=True)
     show_image = models.URLField(null=True, blank=True)
